9c548ee62bd6308a0889fb6073473be805a6ab1a
cesnextdoor <casadastore@gmail.com>
2025-06-10 10:27:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("website ganteng")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "jangan lupa subrek channel gamers ganteng idaman fuck pencitraan nakal tapi tampan."
 )
