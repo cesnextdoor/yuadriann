1be62ca5dd6141f7d0863a0392800748db5e09be
cesnextdoor <casadastore@gmail.com>
2025-06-10 10:28:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("website ganteng")
 st.write(
-    "jangan lupa subrek channel gamers ganteng idaman fuck pencitraan nakal tapi tampan."
+    "jangan lupa Subrek channel gamers ganteng idaman fuck pencitraan nakal tapi tampan."
 )
